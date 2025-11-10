07c3e5e4d5c4ba80119611b1c11ecd17ac36b77e
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-10 17:12:22 -0300

    creacion de home y agregar rutas

---
+++ src/components/home.tsx
@@ -0,0 +1,12 @@
+import CardProducts from "./cardProducts";
+import Header from "./header/header";
+import Footer from "./footer/footer";
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <CardProducts />
+      <Footer />
+    </>
+  );
+}
