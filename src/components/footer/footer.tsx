8cc9e7357c6c1eb274f7a9edcbc91b558c19c1aa
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-10 17:12:11 -0300

    creacion de header y footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        <b>informacion de contacto</b>
+      </p>
+      <p>
+        <a href="">Agroinsumos@gmail.com</a>
+      </p>
+      <p>
+        <a href="">+54291861457</a>
+      </p>
+      <p>Bahia Blanca Springfield 514</p>
+      <button>Politicas</button>
+      <button>Acerca de nosotros</button>
+      <button>Redes sociales</button>
+    </footer>
+  );
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+export default function Header({ loggedIn = false }) {
+  return (
+    <nav className="Nav-header">
+      <button>inicio</button>
+      <button>producto</button>
+      <button>consulta frecuentes</button>
+      <button>contactanos</button>
+      {loggedIn ? (
+        <a title="Perfil" href="login">
+          <span>Perfil</span>
+        </a>
+      ) : (
+        <Link to="/login" title="Perfil">
+          <button>Iniciar Sesion</button>
+        </Link>
+      )}
+      <div>
+        <button>Carrito de compras</button>
+      </div>
+    </nav>
+  );
+}
