4d8aafb785d5ff8e9f281195c7d93334d176c272
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-10 20:19:18 -0300

    ordenamiento de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import "./App.css";
-import Home from "./components/home";
-import Login from "./components/login";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import Register from "./components/register";
-import Profile from "./components/profile";
-import ChangePassword from "./components/changePassword";
-import Recovery from "./components/recovery";
-import Cart from "./components/cart";
-import { Route, Routes } from "react-router-dom";
+import Routs from "./routes";
 
 function App() {
   return (
     <>
       <Header loggedIn={false} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/changePassword" element={<ChangePassword />} />
-        <Route path="/recovery" element={<Recovery />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <Routs />
       <Footer />
     </>
   );
---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+import Register from "./components/register";
+import Profile from "./components/profile";
+import ChangePassword from "./components/changePassword";
+import Recovery from "./components/recovery";
+import Cart from "./components/cart";
+export default function Routs() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/changePassword" element={<ChangePassword />} />
+      <Route path="/recovery" element={<Recovery />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+}
