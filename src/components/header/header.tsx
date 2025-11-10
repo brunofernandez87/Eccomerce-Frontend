96e21e394692d06e948eea4f1d323a9a8a015cae
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-10 19:03:20 -0300

    nuevo logo eccomerce

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/Logo Eccomerce.jpg";
+import logo from "../../assets/Logo eccomerce.jpeg";
 export default function Header({ loggedIn = false }) {
   return (
     <>
