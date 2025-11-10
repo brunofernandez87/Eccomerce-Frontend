d75e24b1c07283a44e0c1551e8ddc2a76e9a403c
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-10 18:57:04 -0300

    agregacion de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import Login from "./components/login";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Register from "./components/register";
+import Profile from "./components/profile";
+import ChangePassword from "./components/changePassword";
+import Recovery from "./components/recovery";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
+      <Header loggedIn={false} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/changePassword" element={<ChangePassword />} />
+        <Route path="/recovery" element={<Recovery />} />
       </Routes>
       <Footer />
     </>
