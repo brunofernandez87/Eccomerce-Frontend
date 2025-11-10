afa4e4b1af8c16387871cc325b9b3f9e071d3e4c
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-10 20:18:44 -0300

    agregar boton de compra

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -4,9 +4,16 @@ const mock = mockProduct[0];
 const mock2 = mockProduct[2];
 export default function Cart() {
   const totally = [mock, mock2];
-  return totally.map((product) => (
-    <div key={product.id_product} className="Product-cart">
-      <CardProduct {...product} cartIN={true} />
-    </div>
-  ));
+  return (
+    <>
+      {totally.map((product) => (
+        <div key={product.id_product} className="Product-cart">
+          <CardProduct {...product} cartIN={true} />
+        </div>
+      ))}
+      <div>
+        <button>Comprar</button>
+      </div>
+    </>
+  );
 }
