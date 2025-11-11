22655712c1523686fe2ecf652b27802c4c4aec34
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-11 11:10:55 -0300

    paginado

--- src/components/cardProducts.tsx
+++ src/components/cardProducts.tsx
@@ -1,23 +1,48 @@
 import productMock from "../mock/productMock.json";
+import { useState } from "react";
+
 export default function CardProducts() {
-  return productMock.map((product) => (
-    <div key={product.id_product} className="Card-Products">
-      <div className="Card-Images">
-        <img src={product.image} alt={product.name} />
+  const [page, setpage] = useState(1);
+  const maxProduct = 5;
+  const limite = page * maxProduct;
+  const limiteant = limite - maxProduct;
+  const products = productMock.slice(limiteant, limite);
+  function handleClickNext() {
+    setpage(page + 1);
+  }
+  function handleClickPrevious() {
+    setpage(page - 1);
+  }
+  return (
+    <>
+      {products.map((product) => (
+        <div key={product.id_product} className="Card-Products">
+          <div className="Card-Images">
+            <img src={product.image} alt={product.name} />
+          </div>
+          <div className="Card-Names">
+            <p>
+              <b>{product.name}</b>
+            </p>
+          </div>
+          <div className="Card-Descriptions">{product.description}</div>
+          <div className="Card-Categories">{product.category}</div>
+          <div className="Card-Prices">
+            <p>
+              <b>Precio:{product.price}</b>
+            </p>
+          </div>
+          <div className="Card-Stocks">stock: {product.stock}</div>
+        </div>
+      ))}
+      <div>
+        {page > 1 && (
+          <button onClick={handleClickPrevious}>Pagina anterior</button>
+        )}
+        {limite < productMock.length && (
+          <button onClick={handleClickNext}>Pagina siguiente</button>
+        )}
       </div>
-      <div className="Card-Names">
-        <p>
-          <b>{product.name}</b>
-        </p>
-      </div>
-      <div className="Card-Descriptions">{product.description}</div>
-      <div className="Card-Categories">{product.category}</div>
-      <div className="Card-Prices">
-        <p>
-          <b>Precio:{product.price}</b>
-        </p>
-      </div>
-      <div className="Card-Stocks">stock: {product.stock}</div>
-    </div>
-  ));
+    </>
+  );
 }
