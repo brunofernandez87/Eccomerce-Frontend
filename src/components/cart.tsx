4cd7f101122559132560e41fb1d36d8a4c965ec3
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-12 20:33:44 -0300

    carrito mantenible

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,32 +1,24 @@
 import CardProduct from "./cardProduct";
 import mockProduct from "../mock/productMock.json";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect } from "react";
 
-export default function Cart() {
-  const mock = mockProduct[0];
-  const mock2 = mockProduct[2];
+export default function Cart({ cartContent, setcartContent }) {
   const { id } = useParams();
-  let product;
-  const initialCart = () => {
-    let totallyfirst = [mock, mock2];
-    if (id) {
-      product = mockProduct.find((p) => p.id_product === parseInt(id));
-      if (product) {
-        totallyfirst = [...totallyfirst, { ...product }];
-      }
+  const product = mockProduct.find((p) => p.id_product === parseInt(id));
+  useEffect(() => {
+    if (product) {
+      setcartContent([...cartContent, product]);
     }
-    return totallyfirst;
-  };
-  const [totally, settotally] = useState(initialCart);
+  }, [product, setcartContent]);
   function handleclick() {
     alert("compra exitosa");
-    settotally([]);
+    setcartContent([]);
   }
 
   return (
     <>
-      {totally.map((product) => (
+      {cartContent.map((product) => (
         <div key={product.id_product} className="Product-cart">
           <CardProduct productID={product.id_product} cartIN={true} />
         </div>
