b8ed660399e6d5fba7fe3d89345388cf1f1e3ab2
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2025-11-25 17:53:01 -0300

    ahora es un map orderdetail

--- src/components/orderDetail.tsx
+++ src/components/orderDetail.tsx
@@ -3,14 +3,17 @@ import orderDetailMock from "../mock/orderDetailMock.json";
 import productoMock from "../mock/productMock.json";
 export default function OrderDetail() {
   const { id } = useParams();
-  const detail = orderDetailMock.find((d) => d.id_order == parseInt(id));
+  const detail = orderDetailMock.filter((d) => d.id_order == parseInt(id));
   const product = productoMock.find((p) => p.id_product == detail?.id_product);
-  //hacer que se pueda recorrer
   return (
     <div>
-      <p> producto:{product?.name}</p>
-      <p>monto:{detail?.amount}</p>
-      <p>precio unitario:{detail?.unit_price}</p>
+      {detail.map((d) => (
+        <div>
+          <p> producto:{product?.name}</p>
+          <p>monto:{d?.amount}</p>
+          <p>precio unitario:{d?.unit_price}</p>
+        </div>
+      ))}
     </div>
   );
 }
